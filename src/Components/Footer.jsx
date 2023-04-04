b9d35a1fa0a6fde16c1d731b4a7a8429787b75aa
fercastaldo <fercastaldo7901@gmail.com>
2023-04-04 15:19:38 -0300

    footer styles

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import './Footer.css'
 
 const Footer = () => {
   return (
     <footer>
-        <p>Powered by</p>
-        <img src="./img/DH.png" alt='DH-logo' />
+       
+        <img src="./images/DH.png" alt='DH-logo' />
+
+ 
+      <div>
+        <img className='socialMedia' src="./images/ico-facebook.png" alt="Facebook Logo" />
+        <img className='socialMedia' src="./images/ico-instagram.png" alt="Instagram Logo" />
+        <img className='socialMedia' src="./images/ico-tiktok.png" alt="Tiktok Logo" />
+        <img className='socialMedia' src="./images/ico-whatsapp.png" alt="Whatsapp Logo" />
+      </div>
+
     </footer>
   )
 }
