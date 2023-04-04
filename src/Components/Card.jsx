e19ffe576451b8ddb58efced722370fc531140ef
fercastaldo <fercastaldo7901@gmail.com>
2023-04-04 15:39:33 -0300

    fav icon added

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -19,9 +19,7 @@ const Card = ({user}) => {
         {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
 
         {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-        <button onClick={addFav} className="favButton">
-         
-          </button>
+        <button onClick={addFav} className="favButton"></button>
     </div>
   );
 };
