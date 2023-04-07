74c8773dc568e7103a840e047ed235f488fd1a2b
fercastaldo <fercastaldo7901@gmail.com>
2023-04-06 21:50:54 -0300

    cardDetail styles

--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -4,7 +4,8 @@ import Form from '../Components/Form'
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Contact = () => {
-  return (
+
+    return (
     <div className='contact'>
       <h2>¿Desea obtener más información?</h2>
       <p>Complete sus datos y lo contactaremos!</p>
--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
@@ -7,11 +7,36 @@ const Detail = () => {
  
   // Consumiendo el parametro dinamico de la URL deberan hacer un fetch a un user en especifico
 
+  const [users, setUsers] = useState({})
+  const {id} = useParams()
+  const url = 'https://jsonplaceholder.typicode.com/users' + id;
+
+  useEffect(() => {
+    fetch(url)
+        .then(res => res.json())
+        .then(data => setUsers(data))
+  }, [])
+
   return (
     <>
-      <h1>Detail Dentist id </h1>
+      <h1>Detail Dentist {users.id} </h1>
       {/* aqui deberan renderizar la informacion en detalle de un user en especifico */}
       {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
+      <table>
+        <tr className='tableCategories'>
+          <td>Nombre</td>
+          <td>Email</td>
+          <td>Teléfono</td>
+          <td>Website</td>
+        </tr>
+        <tr className='tableData'>
+          <td>dato nombre</td> 
+          <td>dato email</td>
+          <td>dato tel</td>
+          <td> dato Website</td>
+        </tr>
+      </table>
+
     </>
   )
 }
